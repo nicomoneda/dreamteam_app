efcdad623b10c94152297d972d2e9e5b2abaa775
Nicolas BRAUN <nico.braun@free.fr>
2021-05-24 17:16:29 +0200

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'dreams#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources: 'dreams' do
+    member do
+      post 'buy', to: 'dreams#buy'
+    end
+  end
+  
+  get 'bought_dreams', to: 'dreams#bought'
+  get 'my_dreams', to: 'dreams#sold'
+  
+  get 'my_profile' to: 'user#profile'
 end
