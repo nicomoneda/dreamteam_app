8f5e1e2f2a298f1d245a3a55b1ea784dcb06e150
Guillaume Doré cd guillaume.dore@protonmail.com <guillaume.dore@protonmail.com>
2021-05-25 11:24:56 +0200

    create view show

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -2,4 +2,13 @@ class DreamsController < ApplicationController
   def index
     @dreams = Dream.all
   end
+
+  def show
+    @dream = Dream.find(params[:id])
+  end
+
+  private
+  def dreams_params
+    params.require(:dream).permit(:name)
+  end
 end
